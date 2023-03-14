024d1a15a0268b55528d8bdeaaf1416c6d7e4374
mikeh995 <mikeh995@hotmail.co.uk>
2023-03-14 17:56:30 +0000

    newone

---
+++ strings.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "orange", "banana", "strawberry"]
+
+print(fruits,"\n")
+
+print(fruits[3].swapcase(), "\n")
+
+print(fruits[2].replace("a", "anananana"), "\n")
+
+print(fruits[0]*20, "\n")
+
+print(fruits[3][2:7].upper(), "\n")
+
+print
