cd3df52e2b059d20f4b33ddd1a26219369dc00be
mikeh995 <mikeh995@hotmail.co.uk>
2023-03-14 17:18:25 +0000

    last update

--- ex11_starter.py
+++ ex11_starter.py
@@ -1,16 +1,16 @@
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
-#3a line of hyphens same length as belgium string
+# 3a line of hyphens same length as belgium string
 
 aa = len(Belgium)
 print('\n', '-' * aa)
 
-#3b string with the comma seperators replaced by colons ':'
+# 3b string with the comma separators replaced by colons ':'
 
 b = Belgium.replace(',', ':')
 print('\n', b)
 
-#3c population of belgium plus population of capital city (answer11183818)
+# 3c population of belgium plus population of capital city (answer11183818)
 
-c= int(Belgium[8:16]) + int(Belgium[26:32])
+c = int(Belgium[8:16]) + int(Belgium[26:32])
 print('\n', c)
